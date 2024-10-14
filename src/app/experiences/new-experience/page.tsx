1bdfe5bd4213e19a464bede1f2a8496fa1551eb5
mersad <mersadhabibi.work@gmail.com>
2024-10-14 11:02:55 +0330

    Fix: overflow-x bug

--- src/app/experiences/new-experience/page.tsx
+++ src/app/experiences/new-experience/page.tsx
@@ -1,8 +1,9 @@
-import { AlertOctagonIcon, CircleAlertIcon } from "lucide-react";
+import BackgroundShapes from "@/components/templates/BackgroundShapes";
+import * as motion from "framer-motion/client";
+import { AlertOctagonIcon } from "lucide-react";
+import { Metadata } from "next";
 import Link from "next/link";
 import NewExperienceForm from "./_components/NewExperienceForm";
-import { Metadata } from "next";
-import BackgroundShapes from "@/components/templates/BackgroundShapes";
 
 export const metadata: Metadata = {
   title: "ثبت تجربه",
@@ -13,7 +14,12 @@ export const metadata: Metadata = {
 export default function NewExperiencePage() {
   return (
     <main className="relative z-10 h-fit bg-white pb-20 pt-10 lg:pb-20 lg:pt-10">
-      <section className="container">
+      <motion.section
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        viewport={{ once: true }}
+        className="container">
         <div className="mb-5 flex h-fit w-full items-center gap-x-2 rounded-lg bg-[#FFCC00] px-4 py-3 font-medium text-gray-4 sm:gap-x-4 sm:px-6 md:text-lg ">
           <AlertOctagonIcon className="size-6 shrink-0 sm:size-7" />
           <p>
@@ -27,7 +33,7 @@ export default function NewExperiencePage() {
           </p>
         </div>
         <NewExperienceForm />
-      </section>
+      </motion.section>
       <BackgroundShapes />
     </main>
   );
