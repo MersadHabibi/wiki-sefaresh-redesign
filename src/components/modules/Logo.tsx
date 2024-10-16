12610722f4d4e8e390a2a42314b22bdf3a0c0bd6
mersad <mersadhabibi.work@gmail.com>
2024-10-16 22:27:21 +0330

    Fix: build bug

--- src/components/modules/Logo.tsx
+++ src/components/modules/Logo.tsx
@@ -1,7 +1,15 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 
-export default function Logo({ className , width = 100 , height = 100 }: { className?: string , width: number, height: number }) {
+export default function Logo({
+  className,
+  width = 100,
+  height = 100,
+}: {
+  className?: string;
+  width?: number;
+  height?: number;
+}) {
   return (
     <>
       <Image
